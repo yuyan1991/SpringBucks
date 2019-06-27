c470110b98a6faaf338cafb091907724ebed60f1
yuyan <yuyan1991@gmail.com>
2019-06-27 21:08:03 +0800

    add simple JPA operations for sample

---
+++ src/main/java/com/qrqs/springbucks/database/repositories/CoffeeRepository.java
@@ -0,0 +1,7 @@
+package com.qrqs.springbucks.database.repositories;
+
+import com.qrqs.springbucks.database.model.Coffee;
+import com.qrqs.springbucks.database.repositories.base.BaseRepository;
+
+public interface CoffeeRepository extends BaseRepository<Coffee, Long> {
+}
