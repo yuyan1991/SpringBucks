2f0ed1548e1d551f7259f938c5322137b04c0567
yuyan <yuyan1991@gmail.com>
2019-06-28 16:22:53 +0800

    for jpa practice only

--- src/main/java/com/qrqs/springbucks/database/repositories/CoffeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.qrqs.springbucks.database.repositories;
-
-import com.qrqs.springbucks.database.model.Coffee;
-import com.qrqs.springbucks.database.repositories.base.BaseRepository;
-
-public interface CoffeeRepository extends BaseRepository<Coffee, Long> {
-}
