e2bec3bd377bd2ded227bd4b55c9ea452e43ff0d
yuyan <yuyan1991@gmail.com>
2019-07-02 21:39:52 +0800

    construct environment for mybatis generator

--- src/main/java/com/qrqs/springbucks/QrqsSpringBucksApplication.java
+++ src/main/java/com/qrqs/springbucks/QrqsSpringBucksApplication.java
@@ -1,10 +1,6 @@
 package com.qrqs.springbucks;
 
-import com.qrqs.springbucks.database.mapper.CoffeeMapper;
-import com.qrqs.springbucks.database.model.Coffee;
 import lombok.extern.slf4j.Slf4j;
-import org.joda.money.CurrencyUnit;
-import org.joda.money.Money;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
@@ -17,26 +13,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @Slf4j
 @MapperScan("com.qrqs.springbucks.database.mapper")
 public class QrqsSpringBucksApplication implements ApplicationRunner {
-	@Autowired
-	private CoffeeMapper coffeeMapper;
-
 	public static void main(String[] args) {
 		SpringApplication.run(QrqsSpringBucksApplication.class, args);
 	}
 
 	@Override
 	public void run(ApplicationArguments args) {
-		Coffee c = Coffee.builder().name("espresso")
-				.price(Money.of(CurrencyUnit.of("CNY"), 20.0)).build();
-		int count = coffeeMapper.save(c);
-		log.info("Save {} Coffee: {}", count, c);
-
-		c = Coffee.builder().name("latte")
-				.price(Money.of(CurrencyUnit.of("CNY"), 25.0)).build();
-		count = coffeeMapper.save(c);
-		log.info("Save {} Coffee: {}", count, c);
-
-		c = coffeeMapper.findById(c.getId());
-		log.info("Find Coffee: {}", c);
 	}
 }
