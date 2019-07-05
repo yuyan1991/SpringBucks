939549ad01172d51d405fd6ab16287a19869f45f
yuyan <yuyan1991@gmail.com>
2019-07-05 17:07:32 +0800

    implement basic CRUD for Mongo DB

--- src/main/java/com/qrqs/springbucks/QrqsSpringBucksApplication.java
+++ src/main/java/com/qrqs/springbucks/QrqsSpringBucksApplication.java
@@ -1,53 +1,70 @@
 package com.qrqs.springbucks;
 
+import com.mongodb.client.result.UpdateResult;
+import com.qrqs.springbucks.database.converter.MoneyReadConverter;
 import com.qrqs.springbucks.database.model.Coffee;
-import com.qrqs.springbucks.database.model.Orders;
-import com.qrqs.springbucks.service.CoffeeService;
-import com.qrqs.springbucks.service.OrdersService;
 import lombok.extern.slf4j.Slf4j;
+import org.joda.money.CurrencyUnit;
+import org.joda.money.Money;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 
-import java.util.List;
-import java.util.Optional;
-
-import static com.qrqs.springbucks.database.model.state.OrderState.INIT;
-import static com.qrqs.springbucks.database.model.state.OrderState.PAID;
+import java.util.*;
 
 @SuppressWarnings({"unused"})
 @SpringBootApplication
 @Slf4j
 public class QrqsSpringBucksApplication implements ApplicationRunner {
 	@Autowired
-	private CoffeeService coffeeService;
-
-	@Autowired
-	private OrdersService ordersService;
+	private MongoTemplate mongoTemplate;
 
 	public static void main(String[] args) {
 		SpringApplication.run(QrqsSpringBucksApplication.class, args);
 	}
 
+	@Bean
+	public MongoCustomConversions mongoCustomConversions() {
+		return new MongoCustomConversions(Collections.singletonList(new MoneyReadConverter()));
+	}
+
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
-		List<Coffee> coffeeList = coffeeService.findAll();
+		Coffee espresso = Coffee.builder()
+								.name("espresso")
+								.price(Money.of(CurrencyUnit.of("CNY"), 20))
+								.createTime(new Date())
+								.updateTime(new Date())
+								.build();
 
-		Optional<Coffee> latte = coffeeService.findByName("latte");
-		log.info("Coffee :: {}", latte);
+		Coffee saved = mongoTemplate.save(espresso);
+		log.info("Coffee :: {}", saved);
 
-		latte = coffeeService.findByName("Latte");
-		log.info("Coffee :: {}", latte);
+		log.info("All Coffee :: ");
+		List<Coffee> coffeeList = mongoTemplate.find(Query.query(Criteria.where("name").is("espresso")), Coffee.class);
+		Optional.ofNullable(coffeeList).ifPresent(
+				coffees -> coffees.forEach(
+									coffee -> log.info("\tCoffee :: {}", coffee)
+				));
 
-		latte = coffeeService.findOneCoffee("Latte");
-		log.info("Coffee :: {}", latte);
 
-		if (latte.isPresent()) {
-			Orders order = ordersService.createOrder("ziqi", latte.get());
-			ordersService.updateState(order, PAID);
-			ordersService.updateState(order, INIT);
-		}
+		Thread.sleep(1000); // 为了看更新时间
+		UpdateResult updateResult = mongoTemplate.updateFirst(
+				Query.query(Criteria.where("name").is("espresso")),
+				new Update().set("price", Money.of(CurrencyUnit.of("CNY"), 30)).currentDate("updateTime"),
+				Coffee.class);
+		log.info("Update Result :: {}", updateResult.getModifiedCount());
+
+		Coffee newUpdatedCoffee = mongoTemplate.findById(saved.getId(), Coffee.class);
+		log.info("New Updated Coffee :: {}", newUpdatedCoffee);
+		Optional.ofNullable(newUpdatedCoffee).ifPresent((updatedCoffee) -> mongoTemplate.remove(updatedCoffee));
 	}
 }
--- src/main/java/com/qrqs/springbucks/database/model/Coffee.java
+++ src/main/java/com/qrqs/springbucks/database/model/Coffee.java
@@ -1,4 +1,27 @@
 package com.qrqs.springbucks.database.model;
 
+import lombok.*;
+import org.joda.money.Money;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+@Builder
+@Data
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Document
 public class Coffee {
+    @Id
+    private String id;
+
+    private String name;
+
+    private Money price;
+
+    private Date createTime;
+
+    private Date updateTime;
 }
