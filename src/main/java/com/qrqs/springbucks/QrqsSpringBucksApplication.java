08921ac318b6bca9fa53052d0acad040afae28cc
yuyan1991 <31606780+yuyan1991@users.noreply.github.com>
2019-07-06 23:31:54 +0800

    add redis sample

--- src/main/java/com/qrqs/springbucks/QrqsSpringBucksApplication.java
+++ src/main/java/com/qrqs/springbucks/QrqsSpringBucksApplication.java
@@ -5,13 +5,23 @@ import com.qrqs.springbucks.database.model.Orders;
 import com.qrqs.springbucks.service.CoffeeService;
 import com.qrqs.springbucks.service.OrdersService;
 import lombok.extern.slf4j.Slf4j;
+import org.joda.money.CurrencyUnit;
+import org.joda.money.Money;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import static com.qrqs.springbucks.database.model.state.OrderState.INIT;
@@ -22,6 +32,12 @@ import static com.qrqs.springbucks.database.model.state.OrderState.PAID;
 @Slf4j
 public class QrqsSpringBucksApplication implements ApplicationRunner {
 	@Autowired
+	private JedisPoolConfig jedisPoolConfig;
+
+	@Autowired
+	private JedisPool jedisPool;
+
+	@Autowired
 	private CoffeeService coffeeService;
 
 	@Autowired
@@ -31,23 +47,33 @@ public class QrqsSpringBucksApplication implements ApplicationRunner {
 		SpringApplication.run(QrqsSpringBucksApplication.class, args);
 	}
 
+	@Bean
+	@ConfigurationProperties("redis")
+	public JedisPoolConfig jedisPoolConfig() {
+		return new JedisPoolConfig();
+	}
+
+	@Bean(destroyMethod = "close")
+	public JedisPool jedisPool(@Value("${redis.host}")  String host) {
+		return new JedisPool(jedisPoolConfig(), host);
+	}
+
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
-		List<Coffee> coffeeList = coffeeService.findAll();
+		log.info("Jedis Pool Config :: {}", jedisPoolConfig);
 
-		Optional<Coffee> latte = coffeeService.findByName("latte");
-		log.info("Coffee :: {}", latte);
+		try (Jedis jedis = jedisPool.getResource()) {
+			coffeeService.findAll().forEach(
+					coffee -> jedis.hset("springbucks_menu",
+											coffee.getName(),
+											coffee.getPrice().getAmount().toString()
+					));
 
-		latte = coffeeService.findByName("Latte");
-		log.info("Coffee :: {}", latte);
+			Map<String, String> menu = jedis.hgetAll("springbucks_menu");
+			log.info("Menus :: {}", menu);
 
-		latte = coffeeService.findOneCoffee("Latte");
-		log.info("Coffee :: {}", latte);
-
-		if (latte.isPresent()) {
-			Orders order = ordersService.createOrder("ziqi", latte.get());
-			ordersService.updateState(order, PAID);
-			ordersService.updateState(order, INIT);
+			String price = jedis.hget("springbucks_menu", "latte");
+			log.info("latte's price is {}", Money.of(CurrencyUnit.of("CNY"), Double.parseDouble(price)));
 		}
 	}
 }
