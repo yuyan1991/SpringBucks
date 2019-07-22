c86c23287e3b2b2b6db4a472d0f5e96a2cc3bb04
yuyan <yuyan1991@gmail.com>
2019-07-22 17:06:27 +0800

    construct reactive redis sample

--- src/main/java/com/qrqs/springbucks/QrqsSpringBucksApplication.java
+++ src/main/java/com/qrqs/springbucks/QrqsSpringBucksApplication.java
@@ -1,9 +1,6 @@
 package com.qrqs.springbucks;
 
-import com.qrqs.springbucks.database.converter.BytesToMoneyConverter;
-import com.qrqs.springbucks.database.converter.MoneyToBytesConverter;
 import com.qrqs.springbucks.database.model.Coffee;
-import com.qrqs.springbucks.service.CoffeeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
@@ -11,35 +8,69 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.core.convert.RedisCustomConversions;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.core.ReactiveHashOperations;
+import org.springframework.data.redis.core.ReactiveStringRedisTemplate;
+import org.springframework.jdbc.core.JdbcTemplate;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
-import java.util.Arrays;
-import java.util.Optional;
+import java.time.Duration;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
 
 @SuppressWarnings({"unused"})
 @SpringBootApplication
 @Slf4j
 public class QrqsSpringBucksApplication implements ApplicationRunner {
-	@Autowired
-	private CoffeeService coffeeService;
+	private static final String KEY = "COFFEE_MENU";
 
-	@Bean
-	public RedisCustomConversions redisCustomConversions() {
-		return new RedisCustomConversions(Arrays.asList(new BytesToMoneyConverter(), new MoneyToBytesConverter()));
-	}
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	@Autowired
+	private ReactiveStringRedisTemplate redisTemplate;
 
 	public static void main(String[] args) {
 		SpringApplication.run(QrqsSpringBucksApplication.class, args);
 	}
 
+	@Bean
+	public ReactiveStringRedisTemplate reactiveRedisTemplate(ReactiveRedisConnectionFactory factory) {
+		return new ReactiveStringRedisTemplate(factory);
+	}
+
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
-		Optional<Coffee> coffee = coffeeService.findCoffeeByCache("mocha");
-		log.info("Coffee :: {}", coffee);
-
-		coffeeService.findCoffeeByCache("mocha");
-		coffee = coffeeService.findCoffeeByCache("mocha");
-
-		log.info("Coffee from Redis :: {}", coffee);
+		ReactiveHashOperations<String, String, String> hashOperations = redisTemplate.opsForHash();
+		CountDownLatch countDownLatch = new CountDownLatch(1);
+		List<Coffee> coffeeList = jdbcTemplate.query("select * from coffee"
+														, (rs, i) ->
+																Coffee.builder()
+																		.id(rs.getLong("id"))
+																		.name(rs.getString("name"))
+																		.price(rs.getLong("price"))
+																		.build()
+														);
+		Flux.fromIterable(coffeeList)
+				.publishOn(Schedulers.single())
+				.doOnComplete(() -> log.info("list ok"))
+				.flatMap(coffee -> {
+							log.info("try to put {}", coffee);
+							return hashOperations.put(KEY, coffee.getName(), coffee.getPrice().toString());
+						})
+				.doOnComplete(() -> log.info("set ok"))
+				.concatWith(redisTemplate.expire(KEY, Duration.ofMinutes(1)))
+				.doOnComplete(() -> log.info("expire ok"))
+				.onErrorResume(e -> {
+					log.error("exception :: {}", e.getMessage());
+					return Mono.just(false);
+				})
+				.subscribe(b -> log.info("Boolean result :: {}", b)
+							, e -> log.info("Error :: {}", e)
+							, () -> countDownLatch.countDown());
+		log.info("Waiting...");
+		countDownLatch.await();
 	}
 }
--- src/main/java/com/qrqs/springbucks/database/model/Coffee.java
+++ src/main/java/com/qrqs/springbucks/database/model/Coffee.java
@@ -1,27 +1,16 @@
 package com.qrqs.springbucks.database.model;
 
-import com.qrqs.springbucks.database.model.base.BaseEntity;
 import lombok.*;
-import org.hibernate.annotations.Type;
-import org.joda.money.Money;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import java.io.Serializable;
-
-@Entity
 @Data
 @Builder
-@ToString(callSuper = true)
-@EqualsAndHashCode(callSuper = true)
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
-public class Coffee extends BaseEntity implements Serializable {
-    @Column(nullable = false)
+public class Coffee {
+    private Long id;
+
     private String name;
 
-    @Column(nullable = false)
-    @Type(type = "org.jadira.usertype.moneyandcurrency.joda.PersistentMoneyMinorAmount",
-            parameters = {@org.hibernate.annotations.Parameter(name = "currencyCode", value = "CNY")})
-    private Money price;
+    private Long price;
 }
